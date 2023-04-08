181cd5f1ba74c1f56edb56db91946edb9cdfb1d1
PetarGTodorov <130036212+PetarGTodorov@users.noreply.github.com>
2023-04-08 10:50:22 +0300

    Delete First_Steps_In_Coding_Lab directory

--- First_Steps_In_Coding_Lab/7.Projects Creations.js
+++
@@ -1,6 +0,0 @@
-function projectsCreations(input){
-    let name = input[0];
-    let numProjects = Number(input[1]);
-    let hoursNeeded = numProjects * 3 ;
-    console.log(`The architect ${name} will need ${hoursNeeded} hours to complete ${numProjects} project/s.`)
-}
--- First_Steps_In_Coding_Lab/8.Pet Shop.js
+++
@@ -1,6 +0,0 @@
-function petShop(input){
-    let dogFood = Number(input[0]);
-    let catFood = Number(input[1]);
-    let totalSum = dogFood*2.5 + catFood*4;
-    console.log(totalSum)
-}
